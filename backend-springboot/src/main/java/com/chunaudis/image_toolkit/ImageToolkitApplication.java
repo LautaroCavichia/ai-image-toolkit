bd841d21b993bb13518f2162485ba41c34968088
Lautaro Cavichia <lautaro-mac-mini@Lautaros-Mac-mini.local>
2025-05-15 20:27:35 +0200

    first commit

---
+++ backend-springboot/src/main/java/com/chunaudis/image_toolkit/ImageToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.chunaudis.image_toolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageToolkitApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/chunaudis/image_toolkit/ImageToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunaudis.image_toolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
